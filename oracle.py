372e216b911579c96dc5d8a5bbbcde94beb103b3
derinhelm <derinhelm@yandex.ru>
2024-01-18 22:44:32 +0300

    Moving score processing and error storing into a single classes

--- oracle.py
+++ oracle.py
@@ -7,88 +7,35 @@ import time
 from constants import LEFT_ARC, RIGHT_ARC, SHIFT, SWAP
 from gnn import GNNNet
 
-class Oracle:
-    def __init__(self, options, irels, device):
-        self.net = GNNNet(options, len(irels), device)
-        self.irels = irels
-        self.train_info = self.create_train_info()
+class Scores:
+    def __init__(self, scrs, uscrs):
+        self.scrs = scrs
+        self.uscrs = uscrs
 
-    def create_train_info(self):
-        t_info = {}
-        t_info["mloss"], t_info["eloss"], t_info["eerrors"], t_info["lerrors"], t_info["etotal"] \
-            = 0.0, 0.0, 0, 0, 0
-        t_info["errs"] = []
-        t_info["iSentence"] = -1
-        t_info["start"] = time.time()
-        return t_info
-
-    def Load(self, epoch):
-        self.net.Load(epoch)
-
-    def Save(self, epoch):
-        self.net.Save(epoch)
-
-
-    def test_evaluate(self, config):
-        """
-        ret = [left arc,
-               right arc
-               shift]
-
-        RET[i] = (rel, transition, score1) for shift, l_arc and r_arc
-         shift = 2 (==> rel=None) ; l_arc = 0; r_acr = 1
-        """
-        scrs, uscrs = self.net.evaluate(config.graph)
-
-        #transition conditions
-        right_arc_conditions = config.check_right_arc_conditions()
-        shift_conditions = config.check_shift_conditions()
-        swap_conditions = config.check_swap_conditions()
-
-        #(avoiding the multiple roots problem: disallow left-arc from root
-        #if stack has more than one element
-        left_arc_conditions = config.check_not_train_left_arc_conditions()
-
-        s1,r1 = max(zip(scrs[2::2], self.irels))
-        s2,r2 = max(zip(scrs[3::2], self.irels))
-        s1 = s1 + uscrs[2]
-        s2 = s2 + uscrs[3]
-        ret = [ [ (r1, LEFT_ARC, s1) ] if left_arc_conditions else [],
-                [ (r2, RIGHT_ARC, s2) ] if right_arc_conditions else [],
-                [ (None, SHIFT, scrs[0] + uscrs[0]) ] if shift_conditions else [] ,
-                [ (None, SWAP, scrs[1] + uscrs[1]) ] if swap_conditions else [] ]
-        return ret
-
-    def create_test_transition(self, config, iSwap, max_swap):
-        scores = self.test_evaluate(config)
-        best = max(chain(*(scores if iSwap < max_swap else scores[:3] )), key = itemgetter(2) )
-        return best
-
-    def calculate_left_scores(self, config, scrs, uscrs):
+    def _calculate_left_scores(self, config, irels):
         left_arc_conditions = config.check_left_arc_conditions()
         if not left_arc_conditions:
             return [], []
         left_cost = config.calculate_left_cost()
-        left_scores = [(rel, LEFT_ARC, scrs[2 + j * 2] + uscrs[2]) \
-                    for j, rel in enumerate(self.irels)]
+        left_scores = [(rel, LEFT_ARC, self.scrs[2 + j * 2] + self.uscrs[2]) \
+                    for j, rel in enumerate(irels)]
         if left_cost == 0:
             left_valid_scores = [(rel, trans, sc) for (rel, trans, sc) in left_scores \
                 if rel == config.get_stack_last_element().relation]
             left_wrong_scores = [(rel, trans, sc) for (rel, trans, sc) in left_scores \
                 if rel != config.get_stack_last_element().relation]
-
         else:
             left_valid_scores = []
             left_wrong_scores = left_scores
         return left_valid_scores, left_wrong_scores
 
-    def calculate_right_scores(self, config, scrs, uscrs):
+    def _calculate_right_scores(self, config, irels):
         right_arc_conditions = config.check_right_arc_conditions()
         if not right_arc_conditions:
             return [], []
         right_cost = config.calculate_right_cost()
-        right_scores = [ (rel, RIGHT_ARC, scrs[3 + j * 2] + uscrs[3]) \
-                    for j, rel in enumerate(self.irels) ]
+        right_scores = [ (rel, RIGHT_ARC, self.scrs[3 + j * 2] + self.uscrs[3]) \
+                    for j, rel in enumerate(irels) ]
 
         if right_cost == 0:
             right_valid_scores = [(rel, trans, sc) for (rel, trans, sc) in right_scores \
@@ -101,14 +48,13 @@ class Oracle:
 
         return right_valid_scores, right_wrong_scores
 
-
-    def calculate_shift_scores(self, config, scrs, uscrs):
+    def _calculate_shift_scores(self, config):
         shift_cost, shift_case = config.calculate_shift_cost()
         shift_conditions = config.check_shift_conditions()
         if not shift_conditions:
              return [], [], shift_case
 
-        shift_scores = [ (None, SHIFT, scrs[0] + uscrs[0]) ]
+        shift_scores = [ (None, SHIFT, self.scrs[0] + self.uscrs[0]) ]
 
         if shift_cost == 0:
             shift_valid_scores = shift_scores
@@ -119,13 +65,13 @@ class Oracle:
 
         return shift_valid_scores, shift_wrong_scores, shift_case
 
-    def calculate_swap_scores(self, config, scrs, uscrs):
+    def _calculate_swap_scores(self, config):
         swap_conditions = config.check_swap_conditions()
         swap_cost = config.calculate_swap_cost()
         if not swap_conditions:
             return [], [], swap_cost
 
-        swap_scores = [(None, SWAP, scrs[1] + uscrs[1])]
+        swap_scores = [(None, SWAP, self.scrs[1] + self.uscrs[1])]
 
         if swap_cost == 0:
             swap_valid_scores = swap_scores
@@ -135,6 +81,89 @@ class Oracle:
             swap_wrong_scores = swap_scores
 
         return swap_valid_scores, swap_wrong_scores, swap_cost
+
+    def create_valid_wrong(self, config, irels):
+        left_valid, left_wrong = self._calculate_left_scores(config, irels)
+        right_valid, right_wrong = self._calculate_right_scores(config, irels)
+        shift_valid, shift_wrong, shift_case = self._calculate_shift_scores(config)
+        swap_valid, swap_wrong, swap_cost = self._calculate_swap_scores(config)
+
+        valid = chain(left_valid, right_valid, shift_valid, swap_valid)
+        wrong = chain(left_wrong, right_wrong, shift_wrong, swap_wrong, [(None, 4, -float('inf'))])
+        # (None, 4, -float('inf')) is used to ensure that at least one element will be.
+        return valid, wrong, shift_case, swap_cost
+
+    def choose_best(self, bestValid, bestWrong, swap_cost, dynamic_oracle):
+        #force swap
+        if swap_cost == 0:
+            best = bestValid
+        else:
+        #select a transition to follow
+        # + aggresive exploration
+        #1: might want to experiment with that parameter
+            if bestWrong[1] == SWAP:
+                best = bestValid
+            else:
+                best = bestValid if ( (not dynamic_oracle) or (bestValid[2] - bestWrong[2] > 1.0) \
+                    or (bestValid[2] > bestWrong[2] and random.random() > 0.1) ) else bestWrong
+
+        return best
+
+    def transition_logging(self, best_valid, best_wrong):
+        transition_logger = getLogger('transition_logger')
+        transition_logger.info("scrs:" + str(self.scrs))
+        transition_logger.info("uscrs:" + str(self.uscrs))
+        transition_logger.info("best_valid:" + str(best_valid) + ", best_wrong:" + str(best_wrong))
+
+    def create_best_transaction(self, config, dynamic_oracle, error_info, irels):
+        valid, wrong, shift_case, swap_cost = self.create_valid_wrong(config, irels)
+        best_valid = max(valid, key=itemgetter(2))
+        best_wrong = max(wrong, key=itemgetter(2))
+        best = self.choose_best(best_valid, best_wrong, swap_cost, dynamic_oracle)
+        error_info.error_append(best, best_valid, best_wrong, config)
+        self.transition_logging(best_valid, best_wrong)
+        return best, shift_case
+
+    def test_evaluate(self, config, irels):
+        """
+        ret = [left arc,
+               right arc
+               shift]
+
+        RET[i] = (rel, transition, score1) for shift, l_arc and r_arc
+         shift = 2 (==> rel=None) ; l_arc = 0; r_acr = 1
+        """
+        #transition conditions
+        right_arc_conditions = config.check_right_arc_conditions()
+        shift_conditions = config.check_shift_conditions()
+        swap_conditions = config.check_swap_conditions()
+
+        #(avoiding the multiple roots problem: disallow left-arc from root
+        #if stack has more than one element
+        left_arc_conditions = config.check_not_train_left_arc_conditions()
+
+        s1,r1 = max(zip(self.scrs[2::2], irels))
+        s2,r2 = max(zip(self.scrs[3::2], irels))
+        s1 = s1 + self.uscrs[2]
+        s2 = s2 + self.uscrs[3]
+        ret = [ [ (r1, LEFT_ARC, s1) ] if left_arc_conditions else [],
+                [ (r2, RIGHT_ARC, s2) ] if right_arc_conditions else [],
+                [ (None, SHIFT, self.scrs[0] + self.uscrs[0]) ] if shift_conditions else [] ,
+                [ (None, SWAP, self.scrs[1] + self.uscrs[1]) ] if swap_conditions else [] ]
+        return ret
+
+class ErrorInfo:
+    def __init__(self):
+        self.train_info = self.create_train_info()
+
+    def create_train_info(self):
+        t_info = {}
+        t_info["mloss"], t_info["eloss"], t_info["eerrors"], t_info["lerrors"], t_info["etotal"] \
+            = 0.0, 0.0, 0, 0, 0
+        t_info["errs"] = []
+        t_info["iSentence"] = -1
+        t_info["start"] = time.time()
+        return t_info
 
     def error_append(self, best, bestValid, bestWrong, config):
         #labeled errors
@@ -156,54 +185,6 @@ class Oracle:
         if best[1] == 0 or best[1] == 2:
             self.train_info["etotal"] += 1
 
-    def create_valid_wrong(self, config, scrs, uscrs):
-        left_valid, left_wrong = self.calculate_left_scores(config, scrs, uscrs)
-        right_valid, right_wrong = self.calculate_right_scores(config, scrs, uscrs)
-        shift_valid, shift_wrong, shift_case = self.calculate_shift_scores(config, scrs, uscrs)
-        swap_valid, swap_wrong, swap_cost = self.calculate_swap_scores(config, scrs, uscrs)
-
-        valid = chain(left_valid, right_valid, shift_valid, swap_valid)
-        wrong = chain(left_wrong, right_wrong, shift_wrong, swap_wrong, [(None, 4, -float('inf'))])
-        # (None, 4, -float('inf')) is used to ensure that at least one element will be.
-        return valid, wrong, shift_case, swap_cost
-
-    def create_best(self, bestValid, bestWrong, swap_cost, config, dynamic_oracle):
-        #force swap
-        if swap_cost == 0:
-            best = bestValid
-        else:
-        #select a transition to follow
-        # + aggresive exploration
-        #1: might want to experiment with that parameter
-            if bestWrong[1] == SWAP:
-                best = bestValid
-            else:
-                best = bestValid if ( (not dynamic_oracle) or (bestValid[2] - bestWrong[2] > 1.0) \
-                    or (bestValid[2] > bestWrong[2] and random.random() > 0.1) ) else bestWrong
-
-        return best
-
-    def create_train_transition(self, config, dynamic_oracle):
-        time_logger = getLogger('time_logger')
-        transition_logger = getLogger('transition_logger')
-
-        ts = time.time()
-        scrs, uscrs = self.net.evaluate(config.graph)
-        time_logger.info(f"Time of net.evaluate: {time.time() - ts}")
-        transition_logger.info("scrs:" + str(scrs))
-        transition_logger.info("uscrs:" + str(uscrs))
-
-        ts = time.time()
-        valid, wrong, shift_case, swap_cost = self.create_valid_wrong(config, scrs, uscrs)
-
-        best_valid = max(valid, key=itemgetter(2))
-        best_wrong = max(wrong, key=itemgetter(2))
-        transition_logger.info("best_valid:" + str(best_valid) + ", best_wrong:" + str(best_wrong))
-        best = self.create_best(best_valid, best_wrong, swap_cost, config, dynamic_oracle)
-        self.error_append(best, best_valid, best_wrong, config)
-        time_logger.info(f"Time of create_best+: {time.time() - ts}")
-        return best, shift_case
-
     def train_logging(self):
         loss_message = (
             f'Processing sentence number: {self.train_info["iSentence"]}'
@@ -218,15 +199,66 @@ class Oracle:
         self.train_info["eerrors"], self.train_info["eloss"], self.train_info["etotal"], self.train_info["lerrors"] = \
             0, 0.0, 0, 0 # TODO: Почему здесь зануляем?
 
-    def error_processing(self, is_final):
-        # len(self.train_info["errs"]) > 50 is used because of footnote 8 in Eli's original paper
-        if (is_final and len(self.train_info["errs"]) > 0) or (len(self.train_info["errs"]) > 50):
-            self.net.error_processing(self.train_info["errs"])
-            self.train_info["errs"] = []
-
     def get_mloss(self):
         return self.train_info['mloss']
 
     def change_sentence_number(self, iSentence):
         self.train_info["iSentence"] = iSentence
+
+    def processing_check(self, is_final):
+        # len(self.train_info["errs"]) > 50 is used because of footnote 8 in Eli's original paper
+        return (is_final and len(self.train_info["errs"]) > 0) or (len(self.train_info["errs"]) > 50)
+
+    def set_errs(self):
+        self.train_info["errs"] = []
+    
+    def get_errs(self):
+        return self.train_info["errs"]
+
+class Oracle:
+    def __init__(self, options, irels, device):
+        self.net = GNNNet(options, len(irels), device)
+        self.irels = irels
+        self.error_info = ErrorInfo()
+
+    def create_test_transition(self, config, iSwap, max_swap):
+        scrs, uscrs = self.net.evaluate(config.graph)
+        scores_info = Scores(scrs, uscrs)
+        scores = scores_info.test_evaluate(config, self.irels)
+        best = max(chain(*(scores if iSwap < max_swap else scores[:3] )), key = itemgetter(2) )
+        return best
+
+    def create_train_transition(self, config, dynamic_oracle):
+        time_logger = getLogger('time_logger')
+
+        ts = time.time()
+        scrs, uscrs = self.net.evaluate(config.graph)
+        time_logger.info(f"Time of net.evaluate: {time.time() - ts}")
+
+        ts = time.time()
+        scores_info = Scores(scrs, uscrs)
+        best, shift_case = scores_info.create_best_transaction(config, dynamic_oracle, self.error_info, self.irels)
+        time_logger.info(f"Time of create_best+: {time.time() - ts}")
+        return best, shift_case
+
+    def error_processing(self, is_final):
+        if self.error_info.processing_check(is_final):
+            errs = self.error_info.get_errs()
+            self.net.error_processing(errs)
+            self.error_info.set_errs()
+
+    def Load(self, epoch):
+        self.net.Load(epoch)
+
+    def Save(self, epoch):
+        self.net.Save(epoch)
+
+    def train_logging(self):
+        self.error_info.train_logging()
+
+    def get_mloss(self):
+        return self.error_info.get_mloss()
+
+    def change_sentence_number(self, iSentence):
+        self.error_info.change_sentence_number(iSentence)
 
