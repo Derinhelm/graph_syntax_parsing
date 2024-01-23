7bac2b203d0a206869ab5ea9d085ff4bd16d302b
derinhelm <derinhelm@yandex.ru>
2024-01-23 22:12:56 +0300

    Changing test configuration processing order

--- project_parser.py
+++ project_parser.py
@@ -1,3 +1,4 @@
+import copy
 from logging import getLogger
 import random
 import time
@@ -16,6 +17,9 @@ class Parser:
         print("device:", self.device)
         self.oracle = Oracle(options, irels, self.device)
         self.embeds = embeds
+        self.info_logger = getLogger('info_logger')
+        self.time_logger = getLogger('time_logger')
+        self.transition_logger = getLogger('transition_logger')
 
     def Load(self, epoch):
         self.oracle.Load(epoch)
@@ -23,13 +27,12 @@ class Parser:
     def Save(self, epoch):
         self.oracle.Save(epoch)
 
-    def test_transaction_processing(self, config, best, max_swap, iSentence, \
+    def test_transition_processing(self, config, best, max_swap, iSentence, \
                                     reached_max_swap, reached_swap_for_i_sentence, iSwap):
-        info_logger = getLogger('info_logger')
         if iSwap == max_swap and not reached_swap_for_i_sentence:
             reached_max_swap += 1
             reached_swap_for_i_sentence = True
-            info_logger.debug(f"reached max swap in {reached_max_swap:d} \
+            self.info_logger.debug(f"reached max swap in {reached_max_swap:d} \
                             out of {iSentence:d} sentences")
         config.apply_transition(best)
         if best[1] == SWAP:
@@ -47,7 +50,6 @@ class Parser:
         return osentence
 
     def Predict(self, data):
-        reached_max_swap = 0
         pbar = tqdm.tqdm(
             data,
             desc="Parsing",
@@ -56,47 +58,67 @@ class Parser:
             leave=False,
             disable=False,
         )
-        res_config_list = []
-        for iSentence, osentence in enumerate(pbar,1):
+        reached_max_swap = 0
+
+        config_to_predict_list = []
+        isentence_config_dict = {} # iSentence -> Configuration
+        for iSentence, osentence in enumerate(data,1):
             config = Configuration(osentence, self.oracle.irels, self.embeds, self.device)
+            # конфигурации в порядке предложений в данных, в процессе работы конфигурации изменяются
+            # в итоге все конфигурации станут итоговыми
+            isentence_config_dict[iSentence] = config 
             max_swap = 2*len(osentence)
             reached_swap_for_i_sentence = False
             iSwap = 0
+            config_to_predict_list.append((config, iSentence, max_swap, \
+                                          reached_swap_for_i_sentence, iSwap))
 
-            while not config.is_end():
+        while len(config_to_predict_list) != 0:
+            new_config_to_predict_list = []
+            for config, iSentence, max_swap, reached_swap_for_i_sentence, iSwap in config_to_predict_list:
                 best = self.oracle.create_test_transition(config, iSwap, max_swap)
                 reached_max_swap, reached_swap_for_i_sentence, iSwap = \
-                        self.test_transaction_processing(config, best, max_swap, iSentence,\
-                                                 reached_max_swap, reached_swap_for_i_sentence, iSwap)
-
-            res_config_list.append(config)
-            print("res_config_list:", res_config_list)
+                        self.test_transition_processing(config, best, max_swap, iSentence,
+                                            reached_max_swap, reached_swap_for_i_sentence, iSwap)
+                if not config.is_end():
+                    new_config_to_predict_list.append((config, iSentence, max_swap, 
+                                                      reached_swap_for_i_sentence, iSwap))
+            config_to_predict_list = new_config_to_predict_list
 
         for iSentence, osentence in enumerate(data,1):
-            config = res_config_list[iSentence - 1]
+            config = isentence_config_dict[iSentence]
             res_osentence = self.create_conll_res(osentence, config)
             yield res_osentence
 
-    def train_transaction_processing(self, config, best, shift_case):
-        time_logger = getLogger('time_logger')
-
+    def train_transition_processing(self, config, best, shift_case):
         #updates for the dynamic oracle
         if self.dynamic_oracle:
             ts = time.time()
             config.dynamic_oracle_updates(best, shift_case)
-            time_logger.info(f"Time of dynamic_oracle_updates: {time.time() - ts}")
+            self.time_logger.info(f"Time of dynamic_oracle_updates: {time.time() - ts}")
 
         ts = time.time()
         config.apply_transition(best)
-        time_logger.info(f"Time of apply_transition: {time.time() - ts}")
+        self.time_logger.info(f"Time of apply_transition: {time.time() - ts}")
         return
+
+    def config_train_processing(self, config):
+        self.transition_logger.info("--------------------")
+        self.transition_logger.info(str(config))
+        ts = time.time()
+        best, shift_case = self.oracle.create_train_transition(config, self.dynamic_oracle)
+        self.time_logger.info(f"Time of create_train_transition: {time.time() - ts}")
+        self.transition_logger.info("best transition:" + str(best))
+        self.train_transition_processing(config, best, shift_case)
+        ts = time.time()
+        self.oracle.error_processing(False)
+        self.time_logger.info(f"Time of error_processing: {time.time() - ts}")
 
     def Train(self, trainData):
         random.shuffle(trainData)
-        info_logger = getLogger('info_logger')
-        time_logger = getLogger('time_logger')
+
         # in certain cases the data will already have been shuffled after being read from file or while creating dev data
-        info_logger.info(f"Length of training data: {len(trainData)}")
+        self.info_logger.info(f"Length of training data: {len(trainData)}")
 
         beg = time.time()
 
@@ -104,35 +126,24 @@ class Parser:
             trainData, desc="Training", unit="sentences",
             mininterval=1.0, leave=False, disable=False,
         )
-        time_logger = getLogger('time_logger')
-        transition_logger = getLogger('transition_logger')
-        config_list = []
+        config_to_predict_list = []
         for sentence in trainData:
             config = Configuration(sentence, self.oracle.irels, self.embeds, self.device)
-            config_list.append(config)
-        while len(config_list) != 0:
-            print("config_list:", config_list)
-            new_config_list = []
-            for config in config_list:
-                transition_logger.info("--------------------")
-                transition_logger.info(str(config))
-                ts = time.time()
-                best, shift_case = self.oracle.create_train_transition(config, self.dynamic_oracle)
-                time_logger.info(f"Time of create_train_transition: {time.time() - ts}")
-                transition_logger.info("best transition:" + str(best))
-                self.train_transaction_processing(config, best, shift_case)
-                ts = time.time()
-                self.oracle.error_processing(False)
-                time_logger.info(f"Time of error_processing: {time.time() - ts}")
+            config_to_predict_list.append(config)
+        while len(config_to_predict_list) != 0:
+            print("config_to_predict_list:", config_to_predict_list)
+            new_config_to_predict_list = []
+            for config in config_to_predict_list:
+                self.config_train_processing(config)
                 if not config.is_end():
-                    new_config_list.append(config)
-            config_list = new_config_list
+                    new_config_to_predict_list.append(config)
+            config_to_predict_list = new_config_to_predict_list
 
         ts = time.time()
         self.oracle.error_processing(True)
-        time_logger.info(f"Time of error_processing: {time.time() - ts}")
+        self.time_logger.info(f"Time of error_processing: {time.time() - ts}")
 
         mloss = self.oracle.get_mloss()
 
-        info_logger.info(f"Loss: {mloss / len(trainData)}")
-        info_logger.info(f"Total Training Time: {time.time() - beg:.2g}s")
+        self.info_logger.info(f"Loss: {mloss / len(trainData)}")
+        self.info_logger.info(f"Total Training Time: {time.time() - beg:.2g}s")
