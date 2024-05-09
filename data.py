20e43b9b66413b3d790db43598b18f855520cdb1
Shamaeva Elena <derinhelm@yandex.ru>
2024-05-09 21:54:06 +0300

    Adding train-a/b/c using flags

--- data.py
+++ data.py
@@ -2,10 +2,20 @@ from utils import set_seeds, read_conll
 from run import run
 from embeddings import create_embeds, load_embeds
 
-def get_real_data(embed_pickle_using, prefix):
-    train_a, train_words_a = read_conll(prefix + 'ru_syntagrus-ud-train-a.conllu')
-    train_b, train_words_b = read_conll(prefix + 'ru_syntagrus-ud-train-b.conllu')
-    train_c, train_words_c = read_conll(prefix + 'ru_syntagrus-ud-train-c.conllu')
+def get_real_data(embed_pickle_using, prefix, train_a_using=True,
+                  train_b_using=True, train_c_using=True):
+    if train_a_using:
+        train_a, train_words_a = read_conll(prefix + 'ru_syntagrus-ud-train-a.conllu')
+    else:
+        train_a, train_words_a = [], set()
+    if train_b_using:
+        train_b, train_words_b = read_conll(prefix + 'ru_syntagrus-ud-train-b.conllu')
+    else:
+        train_b, train_words_b = [], set()
+    if train_c_using:
+        train_c, train_words_c = read_conll(prefix + 'ru_syntagrus-ud-train-c.conllu')
+    else:
+        train_c, train_words_c = [], set()
     train = train_a + train_b + train_c
     val, val_words = read_conll(prefix + 'ru_syntagrus-ud-dev.conllu')
     test, test_words = read_conll(prefix + 'ru_syntagrus-ud-test.conllu')
@@ -28,17 +38,20 @@ def get_short_data(embed_pickle_using, prefix):
     return train, val, test, embeds
 
 def get_data(real_dataset=False, embed_pickle_using=True,
-             colab=False, prefix=""):
+             colab=False, prefix="", train_a_using=True,
+                  train_b_using=True, train_c_using=True):
 # colab=True - run on colab
     if colab:
         if real_dataset:
             return get_real_data(embed_pickle_using, \
-                                 prefix + '/UD_Russian-SynTagRus/')
+                                 prefix + '/UD_Russian-SynTagRus/',
+                                 train_a_using, train_b_using, train_c_using)
         else:
             return get_short_data(embed_pickle_using, \
                                   prefix + '/UD_Russian-SynTagRus-small/')
     else:
         if real_dataset:
-            return get_real_data(embed_pickle_using, prefix + 'UD_Russian-SynTagRus/')
+            return get_real_data(embed_pickle_using, prefix + 'UD_Russian-SynTagRus/',
+                                 train_a_using, train_b_using, train_c_using)
         else:
             return get_short_data(embed_pickle_using, prefix + 'UD_Russian-SynTagRus-small/')
