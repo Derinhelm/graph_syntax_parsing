536534a321bbd04f4e5d65f358a7af849b29c829
Shamaeva Elena <derinhelm@yandex.ru>
2024-10-12 11:39:43 +0300

    Adding test

---
+++ test/test_all.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('..')
+
+from project_logging import logger_creating
+logger_creating()
+
+from utils import set_seeds
+from run import run
+from data import get_data
+
+def test_full_cycle():
+    import os
+    os.system("mkdir models") # TODO
+
+    # really important to do this before anything else to make experiments reproducible
+    set_seeds()
+
+    train, val, test, embeds = get_data(real_dataset=False, embed_pickle_using=True, colab=False)
+
+    run(train, val, test, embeds, epochs=3)
